73e43fa13310be5c64da0a13ea543187ac484045
Caio Lima <caiojlimah@gmail.com>
2021-11-24 19:16:44 -0300

    Requisito 8

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Game from './pages/Game';
 import Login from './pages/Login';
 import Ranking from './pages/Ranking';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
         <Route exact path="/game" render={ () => <Game /> } />
         <Route exact path="/ranking" render={ () => <Ranking /> } />
         <Route exact path="/settings" render={ () => <Settings /> } />
+        <Route exact path="/feedback" render={ () => <Feedback /> } />
       </Switch>
     );
   }
---
+++ src/pages/Feedback.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Feedback extends React.Component {
+  render() {
+    const state = JSON.parse(localStorage.getItem('state'));
+    console.log(state);
+    return (
+      <div>
+        <header>
+          <h1 data-testid="header-player-name">{state.name}</h1>
+          <h3 data-testid="header-score">0</h3>
+          <img data-testid="header-profile-picture" src={ state.gravatarEmail } alt="" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
