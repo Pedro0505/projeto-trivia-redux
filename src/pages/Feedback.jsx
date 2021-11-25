621fe6df99487525d82883dda8ff9b3a6810172c
Pedro Henrique <pedrohrds05@hotmail.com>
2021-11-24 22:39:51 -0300

    Requisito 12 13 16 18

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Feedback extends React.Component {
+  constructor() {
+    super();
+    this.message = this.message.bind(this);
+  }
+
+  message(score) {
+    const limit = 3;
+    if (score < limit) {
+      return <p data-testid="feedback-text">Podia ser melhor...</p>;
+    } return <p data-testid="feedback-text">Mandou bem!</p>;
+  }
+
   render() {
     const state = JSON.parse(localStorage.getItem('state'));
-    console.log(state);
     return (
       <div>
         <header>
-          <h1 data-testid="header-player-name">{state.name}</h1>
-          <h3 data-testid="header-score">0</h3>
-          <img data-testid="header-profile-picture" src={ state.gravatarEmail } alt="" />
+          <h1 data-testid="header-player-name">{state.player.name}</h1>
+          <h3 data-testid="header-score">{ state.player.score }</h3>
+          <img
+            data-testid="header-profile-picture"
+            src={ state.player.gravatarEmail }
+            alt=""
+          />
         </header>
-        <h1 data-testid="feedback-text"> FeedBack </h1>
+        { this.message(state.player.score) }
+        <Link
+          to="/ranking"
+        >
+          <button
+            type="button"
+            data-testid="btn-ranking"
+          >
+            Ver Ranking
+          </button>
+        </Link>
       </div>
     );
   }
